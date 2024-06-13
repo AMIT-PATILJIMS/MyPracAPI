3eb834fadfc7f579221d654b41f04a90590b49fd
AMIT-PATILJIMS <ap1079997@gmail.com>
2024-06-13 10:48:30 +0530

    Implement user authentication filter

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Web.Helpers;
 using System.Web.Http;
+using System.Web.Http.Results;
+using System.Web.Mvc;
 using EmployeeDataAccess;
 
 namespace MyPracAPI.Controllers
 {
     public class EmployeesController : ApiController
     {
+        [BasicAuthentication]
         public IEnumerable<EmployeeData> GetEmployees() 
         {
-            using (MyEmployeeDetailsEntities entities = new MyEmployeeDetailsEntities()) {
-                
+            using (MyEmployeeDetailsEntities entities = new MyEmployeeDetailsEntities()) 
+            {
+                //return entities.EmployeeDatas.ToList();
+                return entities.EmployeeDatas.ToList();
             }
         }
     }
---
+++ EmployeeSecurity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EmployeeDataAccess;
+
+namespace MyPracAPI
+{
+    public class EmployeeSecurity
+    {
+        public static bool LogIn(string Username,string password) 
+        {
+            using (MyEmployeeDetailsEntities entities = new MyEmployeeDetailsEntities())
+            {
+                return entities.Users.Any(user => user.UserName.Equals(Username, StringComparison.OrdinalIgnoreCase)
+                && user.Password.Equals(password, StringComparison.OrdinalIgnoreCase));
+            }
+        }
+    }
+}
